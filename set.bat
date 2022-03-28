7fc29f032e691e759e60f9a0d6ea41033c5ed686
ivanc5po <59787536+ivanc5po@users.noreply.github.com>
2022-03-28 10:33:48 +0800

    Update set.bat

--- set.bat
+++ set.bat
@@ -1,1 +1,1 @@
-"c:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --headless --screenshot=E:\幹\test.png --disable-gpu --window-size=1,1 https://ivanc5po.blogspot.com/2022/03/chong-coin.html
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --headless --screenshot=E:\幹\test.png --disable-gpu --window-size=1,1 https://ivanc5po.blogspot.com/2022/03/chong-coin.html
